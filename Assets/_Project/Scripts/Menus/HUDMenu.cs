c51f31c324a0fff36b8840d6b807f5b985704b6d
FabioJefferson <jeffersonambinintsoafabio@gmail.com>
2023-10-11 11:29:02 +0300

    Update HUDMenu.cs
    Re-add HUDMenu

--- Assets/_Project/Scripts/Menus/HUDMenu.cs
+++ Assets/_Project/Scripts/Menus/HUDMenu.cs
@@ -1,22 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class HUDMenu : SimpleMenu<HUDMenu>
 {
-    //[SerializeField] private Camera _camera;
-
-    public void OnPlayModeButtonPressed()
+    public void OnReplayButtonPressed()
     {
-
+        GameMenu.Show();
     }
-
-
-    public void OnPauseButtonPressed()
+    public void OnExitButtonPressed()
     {
-        PauseMenu.Show();
+        WelcomeMenu.Show();
     }
 }
-
