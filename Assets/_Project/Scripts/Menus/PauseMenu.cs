08e2b9b63bdbac812b6d25bcdd0d5d744eb55cc4
FabioJefferson <jeffersonambinintsoafabio@gmail.com>
2023-10-13 15:27:12 +0300

    Update PauseMenu.cs
    OnBackPressed, send a BoardActionType : show to show the HUDMenu and the Board

--- Assets/_Project/Scripts/Menus/PauseMenu.cs
+++ Assets/_Project/Scripts/Menus/PauseMenu.cs
@@ -1,5 +1,15 @@
-﻿public class PauseMenu : SimpleMenu<PauseMenu>
+﻿using KLab.MessageBuses;
+
+public class PauseMenu : SimpleMenu<PauseMenu>
 {
+
+    public override void OnBackPressed()
+    {
+        base.OnBackPressed();
+        MessageBus.GetBus<BoardAction>().Broadcast(BoardActionType.Show);
+        //MessageBus.GetBus<HUDButtonAction>().Broadcast(HUDButtonActionType.Show);
+    }
+
     public void OnQuitPressed()
     {
         Hide();
