dc598f8da715978262d6423c5865cffe3d48e40f
ericonomena <95394975+ericonomena@users.noreply.github.com>
2023-10-17 11:28:35 +0300

    commit androany
    hehe

--- Assets/_Project/Scripts/Runtime/Messages/MenuBus.cs
+++ Assets/_Project/Scripts/Runtime/Messages/MenuBus.cs
@@ -4,11 +4,3 @@ public sealed class MenuBus : MessageBus<MenuCommands>
 {
     
 }
-
-//public class T : MessageBus<T>
-//{
-//    T Message;
-//    public Menu Instance;
-//    public MenuAction Command;
-//}
-
